065f6b264f1b6ab14ed745c1d0ee1b499703d623
SebastianAndrePacheco <sebastianandre.pq@gmail.com>
2026-04-15 06:05:14 -0500

    fix: add layout to usuarios module

---
+++ app/usuarios/layout.tsx
@@ -0,0 +1,20 @@
+import { SidebarWrapper } from '@/components/layouts/sidebar-wrapper'
+import { Header } from '@/components/layouts/header'
+
+export default function UsuariosLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <div className="flex min-h-screen">
+      <SidebarWrapper />
+      <div className="flex-1 flex flex-col">
+        <Header />
+        <main className="flex-1 p-6 bg-background">
+          {children}
+        </main>
+      </div>
+    </div>
+  )
+}
